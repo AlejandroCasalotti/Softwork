b3de444f2e48a912a7ae513b41e5f38073617557
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-15 03:47:56 +0000

    mass_price_update_supplier

--- mass_price_update/__manifest__.py
+++ mass_price_update/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Mass Price Update',
     'version': '19.0.1.0.0',
-    'category': 'Inventory/Purchase',
+    'category': 'Warehouse',
     'summary': """Actualizar precio unitario del proveedor por porcentaje""",
     'description': """Este módulo permite actualizar masivamente los precios del proveedor""",
     'author': 'SoftWork Arg.',
