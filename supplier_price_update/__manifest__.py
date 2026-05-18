9a1502a5e3bfed9e2c675593038d36f28d860daa
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-18 17:06:52 +0000

    supplier_price_update

--- supplier_price_update/__manifest__.py
+++ supplier_price_update/__manifest__.py
@@ -10,7 +10,7 @@
     'company': 'SW Sistemas',
     'maintainer': 'SoftWork Arg',
     'website': 'https://www.swsistemas.com',
-    'depends': ['product'],
+    'depends': ['supplierinfo'],
     'data': [
         'security/ir.model.access.csv',
         'wizard/increase_price_wizard.xml',
