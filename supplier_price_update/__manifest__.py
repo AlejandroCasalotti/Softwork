9540524f72e2e22d7c9dcd3f68578ca206e44878
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-18 05:25:32 +0000

    sup

--- supplier_price_update/__manifest__.py
+++ supplier_price_update/__manifest__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 {
     'name': 'Aumentar Precio Proveedor',
     'version': '19.0.1.0.0',
@@ -5,11 +7,15 @@
     'summary': 'Permite aumentar precios de proveedores en lote',
     'description': 'Wizard para aumentar precios de productos de proveedores.',
     'author': 'SW Sistemas',
+    'company': 'SW Sistemas',
+    'maintainer': 'SoftWork Arg',
+    'website': 'https://www.swsistemas.com',
     'depends': ['product'],
     'data': [
         'security/ir.model.access.csv',
         'wizard/increase_price_wizard.xml',
     ],
+    'license': 'AGPL-3',
     'installable': True,
     'application': False,
 }
--- supplier_price_update/wizard/__init__.py
+++ supplier_price_update/wizard/__init__.py
@@ -1,1 +1,3 @@
+# -*- coding: utf-8 -*-
+
 from . import increase_price_wizard
