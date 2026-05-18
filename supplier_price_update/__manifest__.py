b43379f47f5630f2de4bd21032e8dd6a05007c90
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-18 05:05:01 +0000

    supplier_price_update

--- supplier_price_update/__manifest__.py
+++ supplier_price_update/__manifest__.py
@@ -3,10 +3,10 @@
 {
     'name': 'Aumentar Precio Proveedor',
     'version': '19.0.1.0.0',
-    'category': 'Inventory',
+    'category': 'Purchase',
     'summary': 'Permite aumentar precios de proveedores en lote',
     'description': 'Wizard para aumentar precios de productos de proveedores.',
-    'author': 'Tu Nombre',
+    'author': 'SW Sistemas',
     'depends': ['product'],
     'data': [
         'security/ir.model.access.csv',
