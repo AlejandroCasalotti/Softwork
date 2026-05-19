1bbdae3673feed7c255915de24ead9dba5876258
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-19 20:29:20 +0000

    sw_coste_net_product

--- sw_coste_net_product/__manifest__.py
+++ sw_coste_net_product/__manifest__.py
@@ -10,7 +10,7 @@
         aplicando descuentos, recargos y tarifas extras en cascada.
     ''',
     'author': 'SW Sistemas',
-    'depends': ['product', 'purchase'],
+    'depends': ['product'],
     'data': [
         'security/ir.model.access.csv',
         'wizard/assign_rule_wizard_views.xml',
