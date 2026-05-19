634767ead3010d18d979f1c9222d396ee8bce98a
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-19 20:02:27 +0000

    sw_coste_net_product

---
+++ sw_coste_net_product/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Costo Neto del Producto',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Calcula costo neto con reglas de descuento y recargo',
+    'description': '''
+        Módulo para calcular el precio neto de productos de proveedores
+        aplicando descuentos, recargos y tarifas extras en cascada.
+    ''',
+    'author': 'Tu Nombre',
+    'depends': ['product'],
+    'data': [
+        'views/coste_net_rule_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
