9b68658df21404016239e368bfa186f1f04cc556
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-20 03:51:31 +0000

    sw_coste_net_product

--- sw_coste_net_product/__manifest__.py
+++ sw_coste_net_product/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Costo Neto del Producto',
-    'version': '1.0',
+    'name': 'Aumentar Precio Proveedor',
+    'version': '19.0.1.0.0',
     'category': 'Purchase',
     'summary': 'Calcula costo neto con reglas de descuento y recargo',
-    'description': '''
-        Módulo para calcular el precio neto de productos de proveedores
-        aplicando descuento, recargo y tarifas en cascada.
-    ''',
-    'author': 'SW sistemas',
-    'depends': ['purchase'],
+    'description': 'Módulo para calcular el precio neto de productos de proveedores',
+    'author': 'SW Sistemas',
+    'company': 'SW Sistemas',
+    'maintainer': 'SoftWork Arg',
+    'website': 'https://www.swsistemas.com',
+    'depends': ['product'],
     'data': [
         'security/ir.model.access.csv',
         'views/coste_net_rule_views.xml',
