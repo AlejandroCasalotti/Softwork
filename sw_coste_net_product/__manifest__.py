001caa9f04a0dc3e22887378ff24b957289e6b79
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-20 03:44:26 +0000

    sw_coste_net_product

--- sw_coste_net_product/__manifest__.py
+++ sw_coste_net_product/__manifest__.py
@@ -15,8 +15,6 @@
         'security/ir.model.access.csv',
         'views/coste_net_rule_views.xml',
     ],
-    'license': 'AGPL-3',
     'installable': True,
-    'auto_install': False,
     'application': False,
 }
