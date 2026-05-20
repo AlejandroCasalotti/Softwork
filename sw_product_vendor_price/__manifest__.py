18ad64cdc11e25be3d3a29e939945a4d40a909c1
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-20 18:03:38 +0000

    sw_product_vendor_price

--- sw_product_vendor_price/__manifest__.py
+++ sw_product_vendor_price/__manifest__.py
@@ -9,7 +9,7 @@
     'company': 'SW Sistemas',
     'maintainer': 'SoftWork Arg',
     'website': 'https://www.swsistemas.com',
-    'depends': ['product', 'purchase'],
+    'depends': ['product'],
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
