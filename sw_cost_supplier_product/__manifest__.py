a347fb8598dbd68facc6e0e98d98f04e901132ec
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-21 03:02:57 +0000

    sw_cost_supplier_product

---
+++ sw_cost_supplier_product/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Costo Proveedor y Margen',
+    'version': '19.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Costo automático desde proveedor y margen de venta',
+    'description': '''
+        Extiende el módulo de reglas de costo:
+        - Costo proveedor: toma el net_price del primer proveedor
+        - Margen de venta: calcula list_price automáticamente
+    ''',
+    'author': 'SW Sistemas',
+    'company': 'SW Sistemas',
+    'maintainer': 'SoftWork Arg',
+    'website': 'https://www.swsistemas.com',
+    'depends': ['product', 'sw_price_rule_supplier'],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
