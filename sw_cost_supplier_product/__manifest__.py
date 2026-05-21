6936933617e57762e0a65cbd106b6a93fdc3caa8
AlejandroCasalotti <ale.cyv@gmail.com>
2026-05-21 04:05:47 +0000

    sw_cost_supplier_product

--- sw_cost_supplier_product/__manifest__.py
+++ sw_cost_supplier_product/__manifest__.py
@@ -15,7 +15,6 @@
     'website': 'https://www.swsistemas.com',
     'depends': ['product', 'sw_price_rule_supplier'],
     'data': [
-        'security/ir.model.access.csv',
         'views/product_views.xml',
     ],
     'license': 'AGPL-3',
