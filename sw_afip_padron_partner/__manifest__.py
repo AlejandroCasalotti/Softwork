910cd20d4f96a85b6ad1e7b5c516963be3ef57ae
AlejandroCasalotti <ale.cyv@gmail.com>
2026-06-07 19:16:06 +0000

    sw_afip_padron_partner

--- sw_afip_padron_partner/__manifest__.py
+++ sw_afip_padron_partner/__manifest__.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'AFIP Test',
+    'name': 'AFIP Padrón Partner',
     'version': '19.0.1.0.0',
+    'category': 'Localization/Argentina',
+    'summary': 'Consulta Padrón AFIP',
     'depends': ['base'],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
     'installable': True,
+    'application': False,
+    'auto_install': False,
 }
--- sw_afip_padron_partner/models/res_partner.py
+++ sw_afip_padron_partner/models/res_partner.py
@@ -1,7 +1,65 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo import _
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
+
+    # Campo AFIP
+    x_afip_cuit = fields.Char(
+        string='AFIP CUIT',
+        help='CUIT para consultar Padrón AFIP',
+    )
     
-    x_test = fields.Char(string='Test')
+    x_estado_padron = fields.Char(
+        string='Estado AFIP',
+        readonly=True,
+    )
+    
+    x_last_update_padron = fields.Date(
+        string='Última Actualización',
+        readonly=True,
+    )
+
+    @api.onchange('x_afip_cuit')
+    def _onchange_afip_cuit(self):
+        """Se ejecuta cuando cambia el CUIT"""
+        if self.x_afip_cuit and len(self.x_afip_cuit) >= 11:
+            # Habilitar botón automáticamente
+            pass
+    
+    def action_update_from_padron_afip(self):
+        """Actualizar desde Padrón AFIP"""
+        self.ensure_one()
+        
+        if not self.x_afip_cuit:
+            raise UserError(_('Debe ingresar el CUIT'))
+        
+        # Limpiar CUIT
+        try:
+            cuit = ''.join(filter(str.isdigit, str(self.x_afip_cuit)))
+        except:
+            raise UserError(_('CUIT inválido'))
+        
+        if len(cuit) != 11:
+            raise UserError(_('El CUIT debe tener 11 dígitos'))
+        
+        # Por ahora, datos de prueba
+        # En producción, aquí iría la conexión a AFIP
+        self.write({
+            'name': 'RAZÓN SOCIAL ACTUALIZADA',
+            'street': 'AV CORRIENTES 1234',
+            'city': 'CAPITAL FEDERAL',
+            'zip': 'C1043',
+            'x_estado_padron': 'ACTIVO',
+            'x_last_update_padron': fields.Date.today(),
+        })
+        
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'res.partner',
+            'res_id': self.id,
+            'view_mode': 'form',
+            'target': 'current',
+        }
