4c2530ca2ef53a7b726f7b44a56d1eeef3759fbb
AlejandroCasalotti <ale.cyv@gmail.com>
2026-06-07 18:56:46 +0000

    sw_afip_padron_partner

--- sw_afip_padron_partner/__manifest__.py
+++ sw_afip_padron_partner/__manifest__.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'AFIP Padrón Partner',
+    'name': 'AFIP Test',
     'version': '19.0.1.0.0',
-    'category': 'Localization/Argentina',
     'depends': ['base'],
-    'data': [
-        'views/res_partner_view.xml',
-        'views/wizard_view.xml',
-        'security/ir.model_access.csv',
-    ],
     'installable': True,
-    'application': False,
 }
--- sw_afip_padron_partner/models/res_partner.py
+++ sw_afip_padron_partner/models/res_partner.py
@@ -3,26 +3,5 @@ from odoo import models, fields
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-
-    x_estado_padron = fields.Char(string='Estado AFIP', readonly=True, copy=False)
-    x_imp_iva_padron = fields.Char(string='IVA AFIP', readonly=True, copy=False)
-    x_imp_ganancias_padron = fields.Char(string='Ganancias AFIP', readonly=True, copy=False)
-    x_last_update_padron = fields.Date(string='Última Actualización AFIP', readonly=True, copy=False)
-
-    def action_update_from_padron_afip(self):
-        self.ensure_one()
-        if not self.vat:
-            from odoo.exceptions import UserError
-            raise UserError('Debe completar el CUIT/CUIL primero')
-        
-        return {
-            'name': 'Actualizar desde Padrón AFIP',
-            'type': 'ir.actions.act_window',
-            'res_model': 'afip.padron.wizard',
-            'view_mode': 'form',
-            'target': 'new',
-            'context': {
-                'default_partner_id': self.id,
-                'default_cuit': self.vat,
-            },
-        }
+    
+    x_test = fields.Char(string='Test')
