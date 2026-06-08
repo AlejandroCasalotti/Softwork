8bc8bde5602662abb0dedd8fccd9b8e4339c7428
AlejandroCasalotti <ale.cyv@gmail.com>
2026-06-08 02:37:39 +0000

    sw_afip_padron

---
+++ sw_l10n_ar_padron_afip/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'AFIP Padrón Partner',
+    'version': '19.0.1.0.0',
+    'category': 'Localization/Argentina',
+    'depends': ['base', 'l10n_ar'],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
