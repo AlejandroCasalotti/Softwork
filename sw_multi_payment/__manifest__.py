2842914434042c4cf1c4a81e49a5dc76fafd8ace
AlejandroCasalotti <ale.cyv@gmail.com>
2026-06-12 03:39:30 +0000

    sw_multi_payment

--- sw_multi_payment/__manifest__.py
+++ sw_multi_payment/__manifest__.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
-
 {
-    'name': 'Aumentar Precio Proveedor',
+    'name': 'Pagos Múltiples',
     'version': '19.0.1.0.0',
-    'category': 'Purchase',
-    'summary': 'Permite aumentar precios de proveedores en lote',
-    'description': 'Wizard para aumentar precios de productos de proveedores.',
+    'category': 'Accounting',
+    'summary': 'Crear pagos con distintos métodos',
+    'description': 'Módulo para multiples métodos de pago juntos',
     'author': 'SW Sistemas',
     'company': 'SW Sistemas',
-    'maintainer': 'SoftWork Arg',
     'website': 'https://www.swsistemas.com',
-    'depends': ['product'],
+    'depends': ['account'],
     'data': [
         'security/ir.model.access.csv',
-        'wizard/increase_price_wizard.xml',
+        'views/payment_view.xml',
     ],
     'license': 'AGPL-3',
     'installable': True,
