2bc45e8cf52af1a66699a74e9f4db77e32cba91d
AlejandroCasalotti <ale.cyv@gmail.com>
2026-06-18 05:33:20 +0000

    sw_automatic_calculation_sale_web

--- sw_automatic_product_calculations/__manifest__.py
+++ sw_automatic_product_calculations/__manifest__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 {
-    'name': 'Cálculos Automáticos',
+    'name': 'Cálculos Automáticos Ventas y Web',
     'version': '19.0.1.0.0',
     'category': 'Sales',
     'summary': 'Calculo de materiales en orden de venta',
@@ -25,3 +23,4 @@
     'installable': True,
     'auto_install': False,
     'application': False,
+}
