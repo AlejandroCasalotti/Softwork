4f6fc0f7ce462edbe51f28de9335542bb954373d
AlejandroCasalotti <ale.cyv@gmail.com>
2026-07-18 02:59:01 +0000

    sw_pr

--- addons/sw_product_cost/models/cost_rule.py
+++ addons/sw_product_cost/models/cost_rule.py
@@ -17,7 +17,7 @@ class SWProductCostRule(models.Model):
     Each line modifies the previous result.
     """
 
-    _name = "cost.rule"
+    _name = "sw.product.cost.rule"
     _description = "Product Cost Rule"
     _order = "sequence, id"
 
