173db89d4cf77b4630c59c67bd48a56397788f70
AlejandroCasalotti <ale.cyv@gmail.com>
2026-07-20 04:07:19 +0000

    softwork_commerce_engine

--- softwork_ecommerce_conector_base/models/sce_odoo_migration.py
+++ softwork_ecommerce_conector_base/models/sce_odoo_migration.py
@@ -115,7 +115,11 @@ class SceOdooMigrationRun(models.Model):
         return uid, models_rpc
 
     def _rpc_call(self, models_rpc, db, uid, pwd, model, method, *args, **kwargs):
-        return models_rpc.execute_kw(db, uid, pwd, model, method, args, kwargs or {})
+        if len(args) == 1 and isinstance(args[0], list):
+            rpc_args = args[0]
+        else:
+            rpc_args = list(args)
+        return models_rpc.execute_kw(db, uid, pwd, model, method, rpc_args, kwargs or {})
 
     def _get_versions(self):
         self.ensure_one()
