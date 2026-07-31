5982b1e7bdf1a3254676634c7c8b579d652be914
AlejandroCasalotti <ale.cyv@gmail.com>
2026-07-31 04:23:31 +0000

    sw_uom_web

--- sw_uom_web/__manifest__.py
+++ sw_uom_web/__manifest__.py
@@ -15,6 +15,7 @@
     "data": [
         "views/product_template_views.xml",
         "views/website_templates.xml",
+        "views/website_sale_cart_templates.xml",
     ],
     "assets": {
         "web.assets_frontend": [
