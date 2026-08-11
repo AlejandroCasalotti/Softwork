52d41dd18823cbea337a170636467cce359fc9b5
AlejandroCasalotti <ale.cyv@gmail.com>
2026-08-11 04:09:02 +0000

    softwork_commerce_engine

--- marketplace_connector/addons/ml_product/models/__init__.py
+++ marketplace_connector/addons/ml_product/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import product_template
+from . import ml_category_search_result
 from . import ml_category_search_wizard
 from . import ml_attribute_option
 from . import ml_listing_type
