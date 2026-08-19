15a7f3ec4021a2c8780e654a427e4c5a0dda9d66
AlejandroCasalotti <ale.cyv@gmail.com>
2026-08-19 05:05:15 +0000

    addons_commerce_engine

--- marketplace_connector/addons/ml_product/__manifest__.py
+++ marketplace_connector/addons/ml_product/__manifest__.py
@@ -8,12 +8,7 @@
     "license": "LGPL-3",
     "depends": ["product", "stock", "softwork_ecommerce_conector_base", "sce_product_marketplace", "sce_connector_ml"],
     "data": [
-        "security/ir.model.access.csv",
         "views/product_template_views.xml",
-        "views/marketplace_publication_views.xml",
-        "views/ml_attribute_editor_wizard_views.xml",
-        "views/ml_publish_config_wizard_views.xml",
-        "views/ml_publish_assistant_wizard_views.xml",
     ],
     "installable": True,
     "application": False
--- marketplace_connector/addons/ml_product/models/__init__.py
+++ marketplace_connector/addons/ml_product/models/__init__.py
@@ -1,10 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from . import product_template
-from . import marketplace_publication
-from . import ml_attribute_editor_wizard
-from . import ml_publish_config_wizard
-from . import ml_publish_assistant_wizard
-from . import ml_publish_assistant_attribute_line
-from . import ml_publish_assistant_saleterm_line
-from . import ml_publish_assistant_picture_line
